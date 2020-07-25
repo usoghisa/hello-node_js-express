c2afdea1b5e18fd16ac6e691d869531df840129c
usoghisa <usoghisa@users.noreply.github.com>
2020-07-25 21:01:10 +0100

    php

--- web.js
+++ web.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-app.get('/', function (req, res) {
-  res.send('index.php');
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(process.env.PORT || 3000);
+
+
