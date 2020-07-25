e59f55df00c8d1340c659093d424c2a5a869539e
usoghisa <usoghisa@users.noreply.github.com>
2020-07-25 21:38:34 +0100

    t

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const router = express.Router();
+const app = express();
 
-// viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+router.get('/',(req, res) => {
+  res.sendfile("index.html");
 });
 
 app.listen(process.env.PORT || 3000);
+
+
 
 
