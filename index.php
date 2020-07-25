e010d3df8869a609d0ebadfb6f7b4d29b67b4c80
usoghisa <usoghisa@users.noreply.github.com>
2020-07-25 21:08:13 +0100

    p

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sample Site</title>
+ </head> 
+  
 <body>
-
+<h1>res.sendFile() Works!</h1>
 <?php
 echo "My first PHP script!";
 ?>
