487c253b45d634b959e75645568729a3483be796
usoghisa <usoghisa@users.noreply.github.com>
2020-07-25 22:13:09 +0100

    s

---
+++ server.js
@@ -0,0 +1,20 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log(`POST request: username is ${username} and password is ${password}`);
+  res.end(`You are now logged in Mr(s) ${username}`);
+});
+
+app.listen(3000, () => {
+  console.log("Started on http://localhost:3000");
+});
