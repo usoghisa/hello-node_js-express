d8b6e2b7176119f344e34429c40acd97a978460c
usoghisa <usoghisa@users.noreply.github.com>
2020-07-25 20:44:38 +0100

    php

--- web.js
+++ web.js
@@ -2,7 +2,16 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello, world  rrrrrrrrrrrrrrrr');
+  res.send('<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!";
+?>
+
+</body>
+</html>');
 });
 
 app.listen(process.env.PORT || 3000);
