3fa9949efd5fc1faee65b520e1ab1ee225169af5
usoghisa <usoghisa@users.noreply.github.com>
2020-07-25 20:52:30 +0100

    php

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('index.html');
+  res.send('index.php');
 });
 
 app.listen(process.env.PORT || 3000);
